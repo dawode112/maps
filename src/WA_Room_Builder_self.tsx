ec040b5e582aa588123060c84dc0fe17f3bc20f1
dawode112 <42343664+dawode112@users.noreply.github.com>
2022-02-12 18:25:51 +0100

    Add files via upload

---
+++ src/WA_Room_Builder_self.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="WA_Room_Builder_self" tilewidth="32" tileheight="32" tilecount="1000" columns="25">
+ <image source="../src/assets/WA_Room_Builder_self.png" width="800" height="1280"/>
+</tileset>
